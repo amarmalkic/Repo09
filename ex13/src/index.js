00fd063ff81f58950d4e948419123dd1be2305a3
wizz <amar.malkic96@gmail.com>
2021-09-21 12:35:08 +0200

    +index

---
+++ ex13/src/index.js
@@ -0,0 +1,13 @@
+function inverseWhile() {
+    var fiveNumbers = "", str = 5;
+    while (str >= 0) {
+        fiveNumbers += str;
+        if (str > 0) {
+            fiveNumbers += ",";
+        }
+        str--;
+    }
+    return fiveNumbers;
+}
+console.log(inverseWhile());
+module.exports = inverseWhile;
